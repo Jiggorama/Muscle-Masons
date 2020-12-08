f57d54a1d79464f0acba3c6c65dc4a8abc8d727c
Thomas Sten <eric@Thomass-MBP.fios-router.home>
2020-12-08 18:44:01 -0500

    basic styling complete

--- src/components/ExerciseDisplay.jsx
+++ src/components/ExerciseDisplay.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import axios from 'axios'
 import { useParams, Link, useHistory } from "react-router-dom"
 import { baseURL, config } from '../services'
+import '../styling/exerciseDisplay.css'
 
 let ExerciseDisplay = (props) => {
   let { id } = useParams()
@@ -19,27 +20,33 @@ let ExerciseDisplay = (props) => {
   } 
 
   return (   
-    <div>
+    <div className='exerciseDisplay'> 
       {exercise ?
-        <div className='ExerciseDisplay'>         
+        <div>         
+          <div className='container'>            
+            <div className = 'exercise'>
             <h1>The {exercise.fields.name}</h1>
             <em>
               <h4>(Targets: {exercise.fields.targeted})</h4>
           </em>
-          <h4>Intensity: {star.repeat(Number(exercise.fields.intensity))}</h4>
-            <div>
-              <h3>Directions</h3>
-            <p>{exercise.fields.directions}</p>
-            <p>{exercise.fields.reps}</p>
-            </div>
+          <h3>Intensity: {star.repeat(Number(exercise.fields.intensity))}</h3>
             <div>
               <h3>Equipment</h3>
               <p>{exercise.fields.equipment}</p>
-          </div> 
+            </div> 
+            </div>
+            <div className= 'directions'>              
+              <h3>Directions</h3>
+            <p>{exercise.fields.directions}</p>
+            <p>For <strong>{exercise.fields.reps}</strong> Reps</p>
+            </div>
+          </div>
+            <div className='buttons'>              
           <Link to={`/edit/${exercise.id}`}>
       <button>Edit</button>
       </Link>
       <button onClick={handleDelete}>Delete</button>
+            </div>
         </div>
         : <h1>loading.....</h1>}
     </div>  
