1f442223c6926ad50e55ba54495b0d6006127c71
Thomas Sten <eric@Thomass-MBP.fios-router.home>
2020-12-11 10:50:24 -0500

    spelling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,7 @@ const Nav = () => {
     <nav className = 'self-end text-gray-100 pb-3 text-sm sm:text-xl'>
       <Link className='hover:bg-orange-400 hover:text-coolGray-800 p-2' to='/'>Home</Link> 
       <Link className='hover:bg-orange-400 hover:text-coolGray-800 p-2' to='/exercise-list'>Exercises</Link> 
-      <Link className='hover:bg-orange-400 hover:text-coolGray-800 p-2' to='/new'>Add-Excercise</Link>
+      <Link className='hover:bg-orange-400 hover:text-coolGray-800 p-2' to='/new'>Add-Exercise</Link>
       <Link className='hover:bg-orange-400 hover:text-coolGray-800 p-2 mr-4' to='/routine'>Routine</Link>
     </nav>
   )
